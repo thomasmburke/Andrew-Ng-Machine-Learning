de7b26ee125bea68d961bf5796bd594029181aaf
thomasmburke <tburke@bu.edu>
2019-02-03 14:15:12 -0500

    read data into numpy matrix before plotting

--- assignment1/plotData.py
+++ assignment1/plotData.py
@@ -6,5 +6,13 @@ def plotData():
              names=('Size (ft^2)', 'Price'), marker='x', plotfuncs={1: 'scatter'})
     plt.show()
 
+
+def loadData():
+    sampleData = np.loadtxt(fname='data/ex1data1.txt', dtype= float, delimiter=',')
+    #print(sampleData)
+    plt.scatter(x=sampleData[:,0],y=sampleData[:,1],marker='x')
+    plt.show()
+
 if __name__=='__main__':
-    plotData()
+    # plotData()
+    loadData()
