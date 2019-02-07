d5a540c77b0a7a89bd7c0c5f8a54322dfd7db60f
thomasmburke <tburke@bu.edu>
2019-02-07 18:38:21 -0500

    making predictions give a population size

--- assignment1/ex1.py
+++ assignment1/ex1.py
@@ -73,22 +73,15 @@ print(' -3.6303\n  1.1664')
 # Plot the linear fit
 plt.scatter(x=X[:,1], y=np.dot(X,theta), marker='o')
 plt.show()
+
+# Predict values for population sizes of 35,000 and 70,000
+predict1 = np.array([1, 3.5]) @ theta
+print('For population = 35,000, we predict a profit of')
+print(predict1*10000)
+predict2 = np.array([1, 7]) @ theta
+print('For population = 70,000, we predict a profit of')
+print(predict2*10000)
 """
-plot(X(:,2), X*theta, '-')
-legend('Training data', 'Linear regression')
-hold off % don't overlay any more plots on this figure
-
-% Predict values for population sizes of 35,000 and 70,000
-predict1 = [1, 3.5] *theta;
-fprintf('For population = 35,000, we predict a profit of %f\n',...
-    predict1*10000);
-predict2 = [1, 7] * theta;
-fprintf('For population = 70,000, we predict a profit of %f\n',...
-    predict2*10000);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 %% ============= Part 4: Visualizing J(theta_0, theta_1) =============
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
 
