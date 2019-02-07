719e424d0bba792defaa708b484f56e1421e8397
thomasmburke <tburke@bu.edu>
2019-02-07 18:44:37 -0500

    added comments

--- assignment1/gradientDescent.py
+++ assignment1/gradientDescent.py
@@ -7,8 +7,8 @@ def gradient_descent(X, y, theta, alpha, iterations):
     for i in range(iterations):
         # Generate a matrix for each steps cost and ensure it is steadily decreasing
         J_history[i] = compute_cost(X, y, theta)
-        # Generate predicted y
-        y_hat = np.dot(X, theta)
+        # Generate predicted y via matrix multiplication
+        y_hat = np.dot(X, theta) # same as X @ theta
         # Calculate next theta values
         theta = theta - (alpha / len(X)) * np.dot(X.T, y_hat - y)
     return J_history, theta
