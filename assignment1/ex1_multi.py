da51d3ff54a2075ae5ab64b06adc501150fa226d
thomasmburke <tburke@bu.edu>
2019-02-11 15:53:43 -0500

    plotted both dimensions against y

--- assignment1/ex1_multi.py
+++ assignment1/ex1_multi.py
@@ -28,7 +28,6 @@ import matplotlib.pyplot as plt
 %% ================ Part 1: Feature Normalization ================
 """
 print('Loading data ...')
-
 # Load Data
 data = np.loadtxt(fname='data/ex1data2.txt', dtype=float, delimiter=',')
 X = data[:, 0:2]
@@ -41,14 +40,13 @@ print(' x = {0}, \n y = {1} \n'.format(X[0:9,:], y[0:9,:]))
 # Scale features and set them to zero mean
 print('Normalizing Features ...\n')
 from featureNormalize import feature_normalize, feature_normalize_stddev
-X_norm = feature_normalize_stddev(X)
-print(X_norm)
-"""
+X = feature_normalize_stddev(X)
+
 # Add intercept term to X
-X = [ones(m, 1) X];
+X = np.hstack(tup=(np.ones(shape=(m,1)), X))
 
-
-%% ================ Part 2: Gradient Descent ================
+"""
+# ================ Part 2: Gradient Descent ================
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: We have provided you with the following starter
@@ -66,49 +64,54 @@ X = [ones(m, 1) X];
 %               Finally, you should complete the code at the end
 %               to predict the price of a 1650 sq-ft, 3 br house.
 %
-% Hint: By using the 'hold on' command, you can plot multiple
-%       graphs on the same figure.
-%
 % Hint: At prediction, make sure you do the same feature normalization.
 %
+"""
 
-fprintf('Running gradient descent ...\n');
+print('Running gradient descent ...\n')
 
-% Choose some alpha value
-alpha = 0.01;
-num_iters = 400;
+# Choose some alpha value
+alpha = 0.01
+iterations = 400
 
-% Init Theta and Run Gradient Descent 
-theta = zeros(3, 1);
-[theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters);
-
-% Plot the convergence graph
+from gradientDescent import gradient_descent
+# Init Theta and Run Gradient Descent 
+theta = np.zeros(shape=(3, 1))
+J_history, theta = gradient_descent(X, y, theta, alpha, iterations)
+print('j_history:\n{}'.format(J_history))
+print('theta:\n{}'.format(theta))
+"""
+# Plot the convergence graph
 figure;
 plot(1:numel(J_history), J_history, '-b', 'LineWidth', 2);
 xlabel('Number of iterations');
 ylabel('Cost J');
-
-% Display gradient descent's result
-fprintf('Theta computed from gradient descent: \n');
-fprintf(' %f \n', theta);
-fprintf('\n');
-
-% Estimate the price of a 1650 sq-ft, 3 br house
-% ====================== YOUR CODE HERE ======================
-% Recall that the first column of X is all-ones. Thus, it does
-% not need to be normalized.
-price = 0; % You should change this
+"""
 
 
-% ============================================================
+# Estimate the price of a 1650 sq-ft, 3 br house
+# Recall that the first column of X is all-ones. Thus, it does
+# not need to be normalized.
+price = np.array([1,1650,3]) @ theta # these prediction values need to be normalized
+# Create 2 subplot, 1 for each variable
+fig, axes = plt.subplots(figsize=(12,4),nrows=1,ncols=2)
+axes[0].scatter(data[:,0],data[:,2],color="b")
+axes[0].set_xlabel("Size (Square Feet)")
+axes[0].set_ylabel("Prices")
+axes[0].set_title("House prices against size of house")
+axes[1].scatter(data[:,1],data[:,2],color="r")
+axes[1].set_xlabel("Number of bedroom")
+axes[1].set_ylabel("Prices")
+axes[1].set_xticks(np.arange(1,6,step=1))
+axes[1].set_title("House prices against number of bedroom")
+# Enhance layout
+plt.tight_layout()
+plt.show()
 
-fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
-         '(using gradient descent):\n $%f\n'], price);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
-%% ================ Part 3: Normal Equations ================
+print('Predicted price of a 1650 sq-ft, 3 br house using gradient descent:\n {}'.format(price))
+"""
+# ================ Part 3: Normal Equations ================
 
 fprintf('Solving with normal equations...\n');
 
