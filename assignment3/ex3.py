db6d821c288e5c0d2273c5f5a689d8c71f8aeb1f
thomasmburke <tburke@bu.edu>
2019-02-16 15:45:39 -0500

    read in data from .mat file

--- assignment3/ex3.py
+++ assignment3/ex3.py
@@ -1,3 +1,4 @@
+"""
 %% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
 
 %  Instructions
@@ -20,21 +21,26 @@
 clear ; close all; clc
 
 %% Setup the parameters you will use for this part of the exercise
-input_layer_size  = 400;  % 20x20 Input Images of Digits
-num_labels = 10;          % 10 labels, from 1 to 10
-                          % (note that we have mapped "0" to label 10)
+"""
+input_layer_size  = 400  # 20x20 Input Images of Digits
+num_labels = 10          # 10 labels, from 1 to 10
+                          # (note that we have mapped "0" to label 10)
 
-%% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset.
-%  You will be working with a dataset that contains handwritten digits.
-%
+# =========== Part 1: Loading and Visualizing Data =============
+#  We start the exercise by first loading and visualizing the dataset.
+#  You will be working with a dataset that contains handwritten digits.
 
-% Load Training Data
-fprintf('Loading and Visualizing Data ...\n')
 
-load('ex3data1.mat'); % training data stored in arrays X, y
-m = size(X, 1);
-
+# Load Training Data
+print('Loading and Visualizing Data ...\n')
+import scipy.io
+data = scipy.io.loadmat('data/ex3data1.mat') # training data stored in arrays X, y
+X = data['X']
+y = data['y']
+print('size of X: {}'.format(X.shape))
+print('size of y: {}'.format(y.shape))
+#m = size(X, 1);
+"""
 % Randomly select 100 data points to display
 rand_indices = randperm(m);
 sel = X(rand_indices(1:100), :);
@@ -86,4 +92,4 @@ pred = predictOneVsAll(all_theta, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
 
-
+"""
