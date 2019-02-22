ec82445054a7c0a399f5b55f8130f0e1350af1c0
thomasmburke <tburke@bu.edu>
2019-02-22 13:39:47 -0500

    created neural network that is 98.14% accurate on training set

--- assignment4/ex4.py
+++ assignment4/ex4.py
@@ -124,6 +124,35 @@ print('Checking Backpropagation... ')
 J, grad1, grad2, reg_J, grad1_reg, grad2_reg = nnCostFunction(initial_nn_params, input_layer_size, hidden_layer_size,
         num_labels, X, y, lambdaValue)
 print(grad1)
+from predict import predict
+def gradientDescentnn(X,y,initial_nn_params,alpha,num_iters,Lambda,input_layer_size, hidden_layer_size, num_labels):
+    """
+    Take in numpy array X, y and theta and update theta by taking num_iters gradient steps
+    with learning rate of alpha
+    
+    return theta and the list of the cost of theta during each iteration
+    """
+    Theta1 = initial_nn_params[:((input_layer_size+1) * hidden_layer_size)].reshape(hidden_layer_size,input_layer_size+1)
+    Theta2 = initial_nn_params[((input_layer_size +1)* hidden_layer_size ):].reshape(num_labels,hidden_layer_size+1)
+    
+    m=len(y)
+    J_history =[]
+    
+    for i in range(num_iters):
+        nn_params = np.append(Theta1.flatten(),Theta2.flatten())
+        cost, grad1, grad2 = nnCostFunction(nn_params,input_layer_size, hidden_layer_size, num_labels,X, y,Lambda)[3:]
+        Theta1 = Theta1 - (alpha * grad1)
+        Theta2 = Theta2 - (alpha * grad2)
+        J_history.append(cost)
+    
+    nn_paramsFinal = np.append(Theta1.flatten(),Theta2.flatten())
+    return nn_paramsFinal , J_history
+nnTheta, nnJ_history = gradientDescentnn(X,y,initial_nn_params,0.8,800,1,input_layer_size, hidden_layer_size, num_labels)
+Theta1 = nnTheta[:((input_layer_size+1) * hidden_layer_size)].reshape(hidden_layer_size,input_layer_size+1)
+Theta2 = nnTheta[((input_layer_size +1)* hidden_layer_size ):].reshape(num_labels,hidden_layer_size+1)
+
+pred3 = predict(Theta1, Theta2, X)
+print("Training Set Accuracy:",sum(pred3[:,np.newaxis]==y)[0]/5000*100,"%")
 """
 # Check gradients by running checkNNGradients
 from checkNNGradients import checkNNGradients
---
+++ assignment4/predict.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sigmoid import sigmoid
+
+def predict(Theta1, Theta2, X):
+    """
+    Summary: Create a feed forward propagation algorithm
+    Theta1 is a 25x401 matrix
+    Theta2 is a 10x26 matrix
+    g(x) = sigmoid function
+    a^(j) = g(X @ theta^(j-1))
+    """
+
+    m, n = X.shape
+    X = np.hstack((np.ones((m,1)),X))
+    a1 = sigmoid(X @ Theta1.T) # yields a 5000x25 matrix
+    # add the bias unit to a2 to ensure dimensions match with the next set of thetas
+    a1 = np.hstack((np.ones((m,1)), a1)) # hidden layer
+    a2 = sigmoid(a1 @ Theta2.T) # output layer yields a 5000x10 matrix
+    return np.argmax(a2,axis=1)+1
