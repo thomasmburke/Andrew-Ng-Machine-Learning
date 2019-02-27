e25591ad3827e84f47249d820bbefc4d2c08d8e2
thomasmburke <tburke@bu.edu>
2019-02-27 15:44:22 -0500

    used prebuilt support vector classifier and plotted result

--- assignment6/ex6.py
+++ assignment6/ex6.py
@@ -31,6 +31,7 @@ print('Loading and Visualizing Data ...')
 data1 = loadmat('data/ex6data1.mat')
 X = data1['X']
 y = data1['y']
+m, n = X.shape
 
 # Plot training data
 pos , neg= (y==1), (y==0)
@@ -52,7 +53,18 @@ plt.show()
 %
 """
 print('Training Linear SVM ...')
-
+from sklearn.svm import SVC
+classifier = SVC(C=1, kernel='linear')
+classifier.fit(X,np.ravel(y))
+plt.figure(figsize=(8,6))
+plt.scatter(X[pos[:,0],0],X[pos[:,0],1],c="r",marker="+",s=50)
+plt.scatter(X[neg[:,0],0],X[neg[:,0],1],c="y",marker="o",s=50)
+# plotting the decision boundary
+X_1,X_2 = np.meshgrid(np.linspace(X[:,0].min(),X[:,1].max(),num=100),np.linspace(X[:,1].min(),X[:,1].max(),num=100))
+plt.contour(X_1,X_2,classifier.predict(np.array([X_1.ravel(),X_2.ravel()]).T).reshape(X_1.shape),1,colors="b")
+plt.xlim(0,4.5)
+plt.ylim(1.5,5)
+plt.show()
 # You should try to change the C value below and see how the decision
 # boundary varies (e.g., try C = 1000)
 """
