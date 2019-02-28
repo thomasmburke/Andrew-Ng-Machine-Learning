d728a1c33abbe7b1704ec4ae6adc5e821d744b40
thomasmburke <tburke@bu.edu>
2019-02-27 20:56:19 -0500

    cleaned up programming exercise

--- assignment6/ex6.py
+++ assignment6/ex6.py
@@ -85,66 +85,43 @@ plt.scatter(X2[pos[:,0],0],X2[pos[:,0],1],c="r",marker="+",s=50)
 plt.scatter(X2[neg[:,0],0],X2[neg[:,0],1],c="y",marker="o",s=50)
 plt.show()
 
-# SVM with a guassian kernel
-# rbf = Radial basis function = gaussian kernel
-classifier2 = SVC(C=1, kernel='rbf',gamma='auto')
-classifier2.fit(X2, np.ravel(y2))
 """
-% Plot training data
-plotData(X, y);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
 %% ========== Part 5: Training SVM with RBF Kernel (Dataset 2) ==========
 %  After you have implemented the kernel, we can now use it to train the 
 %  SVM classifier.
 % 
-fprintf('\nTraining SVM with RBF Kernel (this may take 1 to 2 minutes) ...\n');
+"""
 
-% Load from ex6data2: 
-% You will have X, y in your environment
-load('ex6data2.mat');
+print('Training SVM with RBF Kernel (this may take 1 to 2 minutes) ...')
 
-% SVM Parameters
-C = 1; sigma = 0.1;
-
-% We set the tolerance and max_passes lower here so that the code will run
-% faster. However, in practice, you will want to run the training to
-% convergence.
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-visualizeBoundary(X, y, model);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+# SVM with a guassian kernel
+# rbf = Radial basis function = gaussian kernel
+# by default gamma = 'auto' whish is 1/n for our use we will use 30
+# gamma is effectively equal to 1/sigma
+classifier2 = SVC(C=1, kernel='rbf',gamma=30)
+classifier2.fit(X2, np.ravel(y2))
+"""
 %% =============== Part 6: Visualizing Dataset 3 ================
 %  The following code will load the next dataset into your environment and 
 %  plot the data. 
 %
+"""
+print('Loading and Visualizing Data ...')
 
-fprintf('Loading and Visualizing Data ...\n')
-
-% Load from ex6data3: 
-% You will have X, y in your environment
-load('ex6data3.mat');
-
-% Plot training data
-plotData(X, y);
-
-fprintf('Program paused. Press enter to continue.\n');
-pause;
-
+# Load from ex6data3: 
+# You will have X, y in your environment
+data3 = loadmat('data/ex6data3.mat')
+X3 = data3['X']
+y3 = data3['y']
+m, n = X.shape
+"""
 %% ========== Part 7: Training SVM with RBF Kernel (Dataset 3) ==========
 
 %  This is a different dataset that you can use to experiment with. Try
 %  different values of C and sigma here.
 % 
-
-% Load from ex6data3: 
-% You will have X, y in your environment
-load('ex6data3.mat');
-
+"""
+"""
 % Try different SVM Parameters here
 [C, sigma] = dataset3Params(X, y, Xval, yval);
 
