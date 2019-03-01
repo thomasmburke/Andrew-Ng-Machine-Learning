c430f012999c4dc246602ff90a331d31608b12b2
thomasmburke <tburke@bu.edu>
2019-02-28 22:09:18 -0500

    created get_vocab_list function

--- assignment6/processEmail.py
+++ assignment6/processEmail.py
@@ -1,5 +1,16 @@
-
+import re
 
 def processEmail(email):
     email = email.lower()
+    words = get_vocab_list()
+    print(words)
     return email
+
+def get_vocab_list():
+    with open('data/vocab.txt', mode='r') as myFile:
+        words = myFile.read().splitlines()
+    pattern = r'(\d+\t)(\w+)'
+    cleansedWords = []
+    for word in words:
+        cleansedWords.append(re.search(pattern, word).group(2))
+    return cleansedWords
