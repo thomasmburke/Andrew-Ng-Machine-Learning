ebb2115a9a0bd61ff303b628ed25778e62fa54f3
thomasmburke <tburke@bu.edu>
2019-02-28 22:20:11 -0500

    turned vocab list into a dictionary with numbers

--- assignment6/processEmail.py
+++ assignment6/processEmail.py
@@ -1,16 +1,17 @@
 import re
+from nltk.stem import PorterStemmer
 
 def processEmail(email):
     email = email.lower()
     words = get_vocab_list()
-    print(words)
     return email
 
 def get_vocab_list():
     with open('data/vocab.txt', mode='r') as myFile:
         words = myFile.read().splitlines()
-    pattern = r'(\d+\t)(\w+)'
-    cleansedWords = []
+    pattern = r'(\d+)\t(\w+)'
+    cleansedWords = {}
     for word in words:
-        cleansedWords.append(re.search(pattern, word).group(2))
+        match = re.search(pattern, word)
+        cleansedWords.update({match.group(2): match.group(1)})
     return cleansedWords
