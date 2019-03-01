d873abc3b5e672b8d513de4c50a70e52d7d6c3ff
thomasmburke <tburke@bu.edu>
2019-03-01 15:58:52 -0500

    made prediction on custom email

--- assignment6/ex6_spam.py
+++ assignment6/ex6_spam.py
@@ -101,20 +101,8 @@ for word, index in uniqueWords.items():
         if str(intIndex) == str(index):
             topSpamWords.append(word)
 print(topSpamWords)
+
 """
-# Sort the weights and obtin the vocabulary list
-[weight, idx] = sort(model.w, 'descend');
-vocabList = getVocabList();
-
-fprintf('\nTop predictors of spam: \n');
-for i = 1:15
-    fprintf(' %-15s (%f) \n', vocabList{idx(i)}, weight(i));
-end
-
-fprintf('\n\n');
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
-
 %% =================== Part 6: Try Your Own Emails =====================
 %  Now that you've trained the spam classifier, you can use it on your own
 %  emails! In the starter code, we have included spamSample1.txt,
@@ -122,18 +110,10 @@ pause;
 %  The following code reads in one of these emails and then uses your 
 %  learned SVM classifier to determine whether the email is Spam or 
 %  Not Spam
-
-% Set the file to be read in (change this to spamSample2.txt,
-% emailSample1.txt or emailSample2.txt to see different predictions on
-% different emails types). Try your own emails as well!
-filename = 'spamSample1.txt';
-
-% Read and predict
-file_contents = readFile(filename);
-word_indices  = processEmail(file_contents);
-x             = emailFeatures(word_indices);
-p = svmPredict(model, x);
-
-fprintf('\nProcessed %s\n\nSpam Classification: %d\n', filename, p);
-fprintf('(1 indicates spam, 0 indicates not spam)\n\n');
 """
+# Set the file to be read in (change this to spamSample2.txt,
+# emailSample1.txt or emailSample2.txt to see different predictions on
+# different emails types). Try your own emails as well!
+# Read and predict
+pred = classifier.predict(features.reshape(1,1899))
+print('pred: {}'.format(pred))
